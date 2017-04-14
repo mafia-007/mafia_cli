08ebcafb6f5e3ccccf00d4e78de82a9715549daa
Samuel <clooner0007@gmail.com>
2017-04-14 19:56:26 +0430

    Update leave.lua

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -1,15 +1,14 @@
 local function run(msg, matches)
-BOT = 1324354635 -- your bot id
--- write with @dev_mohammad
+BOT = 248550199 -- your bot id
 if matches[1] == "leave" and is_sudo(msg) then
-		redis:set('lve'..msg.chat_id,'yes')
-		text = "\n<b>/yes\n\n/no</b>"
+		redis:set('leave'..msg.chat_id,'yes')
+		text = "\n/yes\n\n/no"
 		tg.sendMessage(msg.chat_id, 0, 0, text, 0, 'html')
-		elseif matches[1] == "yes" and redis:get('leve'..msg.chat_id)=="yes" then
-tg.changeChatMemberStatus(msg.chat_id, 1324354635, "Left")
-	redis:set('lve'..msg.chat_id_,'no')
+		elseif matches[1] == "yes" and redis:get('leave'..msg.chat_id)=="yes" then
+tg.changeChatMemberStatus(msg.chat_id, 248550199, "Left") -- your bot id
+	redis:set('leave'..msg.chat_id_,'no')
 elseif matches[1] == "no" and redis:get('leave'..msg.chat_id)=="yes" then
-	redis:set('lve'..msg.chat_id_,'no')
+	redis:set('leave'..msg.chat_id_,'no')
 text = "End"
 tg.sendMessage(msg.chat_id, 0, 0, text, 0, 'html')
 end
