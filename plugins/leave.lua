496da65fe3f867e6e2f859a7353d02b5ed94b31d
Samuel <clooner0007@gmail.com>
2017-04-14 19:57:22 +0430

    Update leave.lua

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -1,11 +1,11 @@
 local function run(msg, matches)
-BOT = 248550199 -- your bot id
+BOT = 123456789 -- your bot id
 if matches[1] == "leave" and is_sudo(msg) then
 		redis:set('leave'..msg.chat_id,'yes')
 		text = "\n/yes\n\n/no"
 		tg.sendMessage(msg.chat_id, 0, 0, text, 0, 'html')
 		elseif matches[1] == "yes" and redis:get('leave'..msg.chat_id)=="yes" then
-tg.changeChatMemberStatus(msg.chat_id, 248550199, "Left") -- your bot id
+tg.changeChatMemberStatus(msg.chat_id, 123456789, "Left") -- your bot id
 	redis:set('leave'..msg.chat_id_,'no')
 elseif matches[1] == "no" and redis:get('leave'..msg.chat_id)=="yes" then
 	redis:set('leave'..msg.chat_id_,'no')
