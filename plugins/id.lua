ba357e70a4b5cdeb98eb7fd6e51b5d6a4b68268d
Samuel <clooner0007@gmail.com>
2017-04-14 05:19:10 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -2,7 +2,7 @@
 local function run(msg, matches)
   if matches[1] == 'id' then
 	 
-	 tg.sendMessage(msg.chat_id_, 0, 1,'<b>──────════──────</b> \n📚<b>ID</b> <code>➣➣</code> <b>['..msg.from_id..']</b>\n📚<b>ID</b> <code>➣➣</code> <b>['..msg.chat_id..']</b>\n──────════──────
+	 tg.sendMessage(msg.chat_id_, 0, 1,'<b>──────════──────</b> \n📚<b>ID</b> <code>➣➣</code> <b>['..msg.from_id..']</b>\n📚<b>ID</b> <code>➣➣</code> <b>['..msg.chat_id..']</b>\n──────════──────, 1, 'html')
 	end
     end
 return {
